8cc6da0ce3dae931d0a3df777f8a5514c6ae97a3
Peter Malak <Peter_Malak@khales.com.eg>
2023-11-19 12:31:47 +0200

    adding angular files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './components/footer/footer.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { MainComponent } from './components/main/main.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
-  template: `<h1>Default</h1>`,
-  styleUrls: ['./app.component.css'],
+  imports: [CommonModule, RouterOutlet, FooterComponent, SidebarComponent, MainComponent],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'default';
+  title = 'Portfolio';
 }
---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+// // app.module.ts
+// import { NgModule } from '@angular/core';
+// import { BrowserModule } from '@angular/platform-browser';
+
+// import { AppComponent } from './app.component';
+// import { HelloWorldComponent } from './hello-world.component';
+
+// @NgModule({
+//   declarations: [
+//     AppComponent,
+//     HelloWorldComponent,
+//     // ... other components
+//   ],
+//   imports: [
+//     BrowserModule,
+//     // ... other modules
+//   ],
+//   bootstrap: [AppComponent],
+// })
+// export class AppModule {}
